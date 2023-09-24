b774a9d9695ebe5681b9ee0ea869aa30cb45b2f4
Gayathrimenon123 <120583142+Gayathrimenon123@users.noreply.github.com>
2023-09-24 20:53:28 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace chatpro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Thistle;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.tb3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(496, 513);
+            this.panel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(450, 379);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(97, 460);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(373, 22);
+            this.tb3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 513);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chatpro
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> knowledgeBase;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string defaultMsg= richTextBox1.Text;
+            
+            string msg = tb3.Text;
+            tb3.Clear();
+            richTextBox1.Text = defaultMsg + $"You: {msg}\n";
+            //tb1.AppendText($"You: {msg}\n");
+            defaultMsg = richTextBox1.Text;
+            string response = SimulateChatbotResponse(msg);
+            richTextBox1.Text = defaultMsg +"\n"+ $"Chatbot: {response}\n";
+            //tb1.AppendText($"Chatbot: {response}\n");
+
+           
+        }
+        private string SimulateChatbotResponse(string message)
+        {
+            // Replace this with your chatbot's response logic
+            if (knowledgeBase.ContainsKey(message))
+            {
+                string value= knowledgeBase[message];
+                return value;
+            }
+            else
+            {
+                return "not found";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            knowledgeBase = new Dictionary<string, string>
+        {
+            { "What is your name?", "I am Chatbot, here to assist you!" },
+            { "Hi","Hello there!How can I help you?" },
+            { "Hello","Hello there!How can I help you?" },
+            { "What is the admission age?", "Children aged 3 to 5 are eligible for admission." },
+            { "What documents are required?", "You will need to provide a birth certificate and proof of address." },
+            { "What are the admission fees?", "Admission fees depend on your location. Please visit our website for details." },
+            { "Is financial aid available?", "Yes, we offer financial aid for qualified families. Please contact our admissions office for more information." },
+            { "What is the admission process?", "The admission process includes filling out an application, attending an interview, and providing the necessary documents." },
+            { "Can my child start before turning 3?", "We only admit children who have reached the age of 3 by the start of the school year." },
+            { "Can I schedule a tour?", "Yes, you can schedule a school tour by contacting our admissions office." },
+            { "Default Response", "I'm sorry, I don't have information on that topic. Please contact our admissions office for more details." }
+        
+        //{ "What are the facilities providing here","Play" }
+            // Add more questions and answers here
+        };
+        }
+    }
+}
